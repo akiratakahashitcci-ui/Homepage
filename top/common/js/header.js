4b6024936aa830af24465a8d65ce2adb42ec21f8
localhost0619 <akira_takahashi_tcci@tsuruga.or.jp>
2022-11-16 16:27:13 +0900

    20111116

---
+++ top/common/js/header.js
@@ -0,0 +1,50 @@
+(function($, window, HEADER) {
+    'use strict';
+    HEADER.init();
+    window.HEADER = HEADER;
+})(jQuery, window, {
+    // linkKkakusyusoudan: function() {
+    //     window.location.href = 'top/kakusyusoudan.htm';
+    // },
+    // linkJyouhouka: function() {
+    //     window.location.href = 'top/jyouhouka.htm';
+    // },
+    // linkKyosaiFukuri: function() {
+    //     window.location.href = 'top/kyosaifukuri.htm';
+    // },
+    // linkKentei: function() {
+    //     window.location.href = 'top/kentei.htm';
+    // },
+    // linkKaikanriyo: function() {
+    //     window.location.href = 'top/kaikanriyo/index.html';
+    // },
+    // linkKaihou: function() {
+    //     window.location.href = 'top/kaihou.htm';
+    // },
+    // linkMachidukuri: function() {
+    //     window.location.href = 'top/machidukuri.htm';
+    // },
+    // init: function() {
+    //     $(document).on('click', '.consultation', function() {
+    //         HEADER.linkKkakusyusoudan();
+    //     });
+    //     $(document).on('click', '.informatization', function() {
+    //         HEADER.linkJyouhouka();
+    //     });
+    //     $(document).on('click', '.welfare', function() {
+    //         HEADER.linkKyosaiFukuri();
+    //     });
+    //     $(document).on('click', '.exam', function() {
+    //         HEADER.linkKentei();
+    //     });
+    //     $(document).on('click', '.conference-room', function() {
+    //         HEADER.linkKaikanriyo();
+    //     });
+    //     $(document).on('click', '.newsletter', function() {
+    //         HEADER.linkKaihou();
+    //     });
+    //     $(document).on('click', '.town-planning', function() {
+    //         HEADER.linkMachidukuri();
+    //     });
+    // }
+});
